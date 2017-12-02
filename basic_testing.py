2e4edb7020b1062008d7d45e323f7e90142ce7cc
Nick Hartley (Contractor) <nick.hartley@avalara.com>
2017-12-02 08:12:03 -0500

    Adding .gitignore file.

--- basic_testing.py
+++
@@ -1,8 +0,0 @@
-addr = ['124', 'Conch', 'Street', 'Bikini', 'Bottom', 'CA', 'US', '27716']
-output = ''
-for x in range(0, len(addr)):
-	output += addr[x]
-	
-	if x != len(addr) - 1:
-		output += ' '
-print(output)
